fa100cf7ee0eab1b8f9b31535a4d360bad24361a
zkenz7x <57540757+zkenz7x@users.noreply.github.com>
2021-10-26 12:18:00 +0100

    TestApplicationFor2i-coding
    2i coding challenge

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 53;
+            while (number < 96)
+            {
+                if (number % 7 == 0)
+                    Console.WriteLine(number);
+
+                number++;
+            }
+
+
+        }
+    }
+}
